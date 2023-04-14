eeed5234ce41d48cc4872f864c1eef7131daf846
CasperSidor <casper.sidor@gmail.com>
2023-04-14 12:17:00 +0200

    trying to make unref work, not working yet

---
+++ shopui/src/products.js
@@ -0,0 +1,24 @@
+import product1 from './assets/Anders.jpg'
+import product2 from './assets/Eragon.jpg'
+import product3 from './assets/Krimi.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Tegneserie",
+        price: 4.95,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Fantasy",
+        price: 29.95,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Krimi",
+        price: 44.95,
+        productImage: product3,
+    },
+];
